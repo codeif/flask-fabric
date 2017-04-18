f9153ccceaba1711ac11367eebef24e130f2e8a9
codeif <me@codeif.com>
2017-04-18 18:04:51 +0800

    install python3-dev replace python3

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def install_pip():
     if not exists('/usr/bin/python'):
         sudo('apt-get -y install python')
     if not exists('/usr/bin/python3'):
-        sudo('apt-get -y install python3')
+        sudo('apt-get -y install python3-dev')
     if not exists('/usr/local/bin/pip'):
         run('curl --silent --show-error --retry 3 '
             'https://bootstrap.pypa.io/get-pip.py | '
